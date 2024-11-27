1448b898651cc5f232480bcdd279e65d9076ee77
Basttylol <bastty321@hotmail.com>
2024-11-27 00:26:36 -0300

    ultima version del prototipo antes de entregarlo

--- server/app.js
+++ server/app.js
@@ -24,7 +24,6 @@ app.use('/resources', express.static(__dirname + '/public'));
 // Invocamos bcryptjs
 const bcryptjs = require('bcryptjs');
 
-const jwt = require('jsonwebtoken')
 
 // Variables de sesión
 const session = require('express-session');
@@ -129,7 +128,38 @@ app.get('/session', (req, res) => {
 });
 
 app.get('/odontologos', (req,res)=>{
-    conexion.query('SELECT * FROM odontologos', async (error, results)=>{
+    conexion.query(
+        `
+        SELECT 
+        o.id_odontologo,
+        u.id_usuarios,
+        u.nombre,
+        u.apellido,
+        o.sucursal,
+        o.especialidad
+        FROM usuarios u
+        INNER JOIN odontologos o ON o.id_usuarios = u.id_usuarios
+        `, async (error, results)=>{
+        if (results.length == 0){
+            console.log('no existen odontologos')
+        }else{
+            res.json(results);
+        }
+    })
+})
+
+app.get('/usuarios', (req, res) => {
+    conexion.query(
+    `
+    SELECT 
+    id_usuarios,
+    rut,
+    nombre, 
+    apellido,
+    correo, fecha_registro
+    FROM usuarios
+    `
+    , async (error, results)=>{
         if (results.length == 0){
             console.log('no existen odontologos')
         }else{
@@ -160,14 +190,9 @@ app.get('/sucursales', (req,res)=>{
     })
 })
 
-// app.post('/reservar-hora', (req,res)=>{
-//     const {fecha, hora, id_usuarios}
-// })
-
 app.post('/pacientes', (req,res)=>{
     const {id_usuarios} = req.body;
-    console.log(req.body)
-    console.log('ID USUARIO EN HORAS:', id_usuarios)
+    console.log(id_usuarios)
     conexion.query(
         `
         SELECT p.id_paciente, u.nombre, u.rut
@@ -183,6 +208,46 @@ app.post('/pacientes', (req,res)=>{
         }
     })
 })
+
+app.post('/id-odontologos', (req, res )=>{
+    const {id_usuarios} = req.body;
+    conexion.query(
+        `
+        SELECT o.id_odontologo
+        FROM usuarios u
+        INNER JOIN odontologos o ON o.id_usuarios = u.id_usuarios
+        WHERE u.id_usuarios = ?;
+        `,[id_usuarios]
+        , async (error, results)=>{
+        if (results.length == 0){
+            console.log('No existen odontologos')
+        }else{
+            res.json(results);
+        }
+    })
+})
+
+app.post('/id-pacientes', (req, res )=>{
+    const {id_usuarios} = req.body;
+    conexion.query(
+        `
+        SELECT p.id_paciente
+        FROM usuarios u
+        INNER JOIN pacientes p ON p.id_usuarios = u.id_usuarios
+        WHERE u.id_usuarios = ?;
+        `,[id_usuarios]
+        , async (error, results)=>{
+        if (results.length == 0){
+            console.log('No existen pacientes id-pacientes')
+        }else{
+            res.json(results);
+        }
+    })
+})
+
+
+
+
 app.post('/filtro-especialidades', (req,res)=>{
     const {especialidad} = req.body;
     conexion.query(`
@@ -227,10 +292,9 @@ app.post('/filtro-sucursales', (req,res)=>{
 
 app.post('/horas', (req,res)=>{
     const {fecha, id_odontologo} = req.body
-    console.log(fecha, id_odontologo)
     conexion.query(
         `
-        SELECT horas.hora, fechas.fecha
+        SELECT horas.hora, fechas.fecha, horas.id_horas
         FROM fechas
         INNER JOIN horas ON horas.id_odontologo = fechas.id_odontologo AND horas.id_fecha = fechas.id_fecha
         WHERE fechas.fecha = ? AND fechas.id_odontologo = ? AND horas.disponibilidad = "DISPONIBLE"`,
@@ -259,6 +323,142 @@ app.get('/horas-disponibles', (req,res)=>{
             res.json(results);
         }
     })
+})
+
+app.put('/registrar-hora', (req,res) =>{
+    const {id_horas} = req.body;
+    conexion.query(
+    `
+    UPDATE horas SET disponibilidad = "NO_DISPONIBLE" WHERE id_horas = ?
+    `, [id_horas], async (err, results) =>{
+        if (results.length == 0){
+
+        }else{
+            res.json({ success: true, message: 'Reservaste tu hora!', icon:'success'})
+        }
+    })
+})
+
+app.put('/confirmar-cita',(req,res)=>{
+    const {id_citas} = req.body;
+
+    conexion.query(
+    `
+    UPDATE citas SET estado = "CONFIRMADA" WHERE citas.id_citas = ?;
+    `, [id_citas], async (err, results) =>{
+        if (results.length == 0){
+        }else{
+            res.json({ success: true, message: 'confirmaste tu cita!', icon:'success'})
+        }
+    })
+})
+
+app.put('/cancelar-cita',(req,res)=>{
+    const {id_citas} = req.body;
+
+    conexion.query(
+    `
+    UPDATE citas SET estado = "CANCELADA" WHERE citas.id_citas = ?;
+    `, [id_citas], async (err, results) =>{
+        if (results.length == 0){
+        }else{
+            res.json({ success: true, message: 'cancelaste tu cita!', icon:'success'})
+        }
+    })
+})
+
+
+app.post('/registrar-citas', (req,res)=>{
+    const {id_horas, id_paciente, id_odontologo} = req.body
+    conexion.query(
+        `
+        INSERT INTO citas (id_paciente, id_odontologo, id_horas) VALUES (?,?,?)
+        `,[id_paciente, id_odontologo, id_horas],
+        (error, results) => {
+            if (error) {
+                return res.status(500).json({ error: "Error en el servidor" });
+            }
+            if (results.length === 0) {
+                return res.json({ success: false, message: 'Error', icon:'warning'});
+            }
+            res.json({ success: true, message: 'Reservaste tu cita', icon:'success'});
+        }
+    );
+})
+
+app.post('/obtener-citas-odontologos', (req, res)=>{
+    const {id_odontologo} = req.body
+    conexion.query(
+    `   
+    SELECT        
+		c.estado AS estado_cita,
+        h.hora AS hora_cita,
+        u.nombre AS nombre_usuario,
+        u.apellido AS apellido_usuario,
+        f.fecha AS fecha_cita
+    FROM 
+        citas c
+    JOIN 
+        horas h ON c.id_horas = h.id_horas
+    JOIN 
+        fechas f ON h.id_fecha = f.id_fecha
+    JOIN 
+        odontologos o ON f.id_odontologo = o.id_odontologo
+    JOIN 
+        pacientes p ON p.id_paciente  = c.id_paciente
+    JOIN
+    	usuarios u ON u.id_usuarios = p.id_usuarios
+    WHERE 
+        o.id_odontologo = ?;
+        
+    `,[id_odontologo],
+        (error, results) => {
+            if (results.length === 0) {
+            }
+            res.json(results);
+        }
+    )
+})
+
+app.post('/obtener-citas-pacientes', (req, res)=>{
+    const {id_paciente} = req.body
+    conexion.query(
+    `   
+    SELECT        
+        c.estado AS estado_cita,
+        h.hora AS hora_cita,
+        u_paciente.nombre AS nombre_usuario,          -- Nombre del paciente
+        u_paciente.apellido AS apellido_usuario,      -- Apellido del paciente
+        f.fecha AS fecha_cita,
+        u_odontologo.nombre AS nombre_odontologo,     -- Nombre del odontólogo
+        u_odontologo.apellido AS apellido_odontologo, -- Apellido del odontólogo
+        o.especialidad,
+        o.sucursal,
+        c.id_citas
+    FROM 
+        citas c
+    JOIN 
+        horas h ON c.id_horas = h.id_horas
+    JOIN 
+        fechas f ON h.id_fecha = f.id_fecha
+    JOIN 
+        pacientes p ON p.id_paciente = c.id_paciente
+    JOIN
+        usuarios u_paciente ON u_paciente.id_usuarios = p.id_usuarios  -- Datos del paciente
+    JOIN
+        odontologos o ON o.id_odontologo = c.id_odontologo  -- Datos del odontólogo (especialidad, sucursal)
+    JOIN
+        usuarios u_odontologo ON u_odontologo.id_usuarios = o.id_usuarios  -- Datos del odontólogo (nombre, apellido)
+    WHERE 
+        p.id_paciente = ?;
+        
+    `,[id_paciente],
+        (error, results) => {
+            if (results.length === 0) {
+            }
+            res.json(results);
+        }
+    )
 })
 
 
